97880c3d386d7e770c49683b6878b14e07d8f9cd
Vallen <valenthisside@gmail.com>
2025-02-01 00:29:14 +0530

    server clean up

--- jeru_bike/server.lua
+++ jeru_bike/server.lua
@@ -1,12 +1,10 @@
 local attachedBikes = {}
 
-RegisterServerEvent('l-sitbehindbike:server:SetBikeData')
-AddEventHandler('l-sitbehindbike:server:SetBikeData', function(networkid, val)
+RegisterServerEvent('l-sitbehindbike:server:SetBikeData', function(networkid, val)
     attachedBikes[networkid] = val
     TriggerClientEvent("l-sitbehindbike:client:SyncData", -1, attachedBikes)
 end)
 
-RegisterServerEvent('l-sitbehindbike:server:RequestData')
-AddEventHandler('l-sitbehindbike:server:RequestData', function()
+RegisterServerEvent('l-sitbehindbike:server:RequestData', function()
     TriggerClientEvent("l-sitbehindbike:client:SyncData", -1, attachedBikes)
 end)
